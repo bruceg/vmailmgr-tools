c32214d4a743d3fe6718a9aa8161960608e99f04
Bruce Guenter <bruce@untroubled.org>
2004-02-11 20:19:45 +0000

    Multi-line strings are now depricated by modern compilers.

--- vcheckquota.c
+++ vcheckquota.c
@@ -33,10 +33,10 @@
 const char program[] = "vcheckquota";
 const int msg_show_pid = 0;
 const char cli_help_prefix[] = "vmailmgr quota enforcement program\n";
-const char cli_help_suffix[] = "
-Warning: the soft-message is linked into the users maildir once for each
-message that is received while the account is over its soft quota.  This may
-result in multiple warning messages.\n";
+const char cli_help_suffix[] = "\n"
+"Warning: the soft-message is linked into the users maildir once for each\n"
+"message that is received while the account is over its soft quota.  This may\n"
+"result in multiple warning messages.\n";
 const char cli_args_usage[] = "";
 const int cli_args_min = 0;
 const int cli_args_max = 0;
